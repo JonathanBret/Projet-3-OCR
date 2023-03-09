2685e4416831ae279ba80a74881b08ac6351eec5
JonathanBret <bret.jonathan@outlook.fr>
2023-03-09 16:23:53 +0100

    Afiichage API

--- Backend/script.js
+++ Backend/script.js
@@ -2,12 +2,14 @@ fetch('http://localhost:5678/api/works')
   .then(response => {
     return response.json();
   })
+
   .then(data => {
     for (let i = 0; i < data.length; i++) {
+      const imagediv = document.querySelector('.gallery');
       const imageUrl = data[i].imageUrl;
       const img = document.createElement('img');
       img.src = imageUrl;
-      document.body.appendChild(img);
+      imagediv.appendChild(img);
     }
   })
   .catch(error => {
