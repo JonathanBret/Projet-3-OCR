3debe37b01c86bd7a790d2b6557ab08dee6459d0
JonathanBret <bret.jonathan@outlook.fr>
2023-03-21 17:05:40 +0100

    avancement

--- Backend/script.js
+++ Backend/script.js
@@ -1,17 +1,77 @@
-fetch('http://localhost:5678/api/works')
-  .then(response => {
-    return response.json();
-  })
 
+fetch('http://localhost:5678/api/works')
+  .then(response => response.json())
   .then(data => {
-    for (let i = 0; i < data.length; i++) {
-      const imagediv = document.querySelector('.gallery');
-      const imageUrl = data[i].imageUrl;
-      const img = document.createElement('img');
-      img.src = imageUrl;
-      imagediv.appendChild(img);
-    }
+    data.forEach(item => {
+      let figure = document.createElement('figure');
+      let image = document.createElement('img');
+      let text = document.createElement('figcaption');
+      
+      image.src = item.imageUrl;
+      text.textContent = item.title;
+      
+      figure.appendChild(image);
+      figure.appendChild(text);
+    
+      document.querySelectorAll('.gallery').forEach(div => {
+        div.appendChild(figure);
+      });
+    });
   })
-  .catch(error => {
-    console.error('Erreur :', error);
+  .catch(error => console.error(error));
+
+
+  const apiUrl = 'http://localhost:5678/api/works';
+
+const getData = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const filterData = (data, category) => {
+  if (category === 'all') {
+    return data;
+  } else {
+    return data.filter(item => item.categoryId === parseInt(category));
+  }
+};
+
+const displayData = (data) => {
+  const gallery = document.querySelector('.gallery');
+
+  gallery.innerHTML = data.map(item => `
+    <div class="gallery-item">
+      <img src="${item.imageUrl}" alt="${item.title}">
+      <div class="gallery-item-text">${item.title}</div>
+    </div>
+  `).join('');
+
+  const filterButtons = document.querySelectorAll('.filter-button');
+  filterButtons.forEach(button => {
+    button.addEventListener('click', async () => {
+      const category = button.dataset.category;
+      const filteredData = filterData(data, category);
+      gallery.innerHTML = filteredData.map(item => `
+        <div class="gallery-item">
+          <img src="${item.imageUrl}" alt="${item.title}">
+          <div class="gallery-item-text">${item.title}</div>
+        </div>
+      `).join('');
+    });
   });
+};
+
+
+const init = async () => {
+  const data = await getData();
+  displayData(data);
+};
+
+init();
+
+
