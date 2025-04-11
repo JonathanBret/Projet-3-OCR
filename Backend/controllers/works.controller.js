8bc84e34fb3187cde004cb6ddcfa6ca4a26589ea
JonathanBret <bret.jonathan@outlook.fr>
2025-04-12 00:31:08 +0200

    fix: update url image

--- Backend/controllers/works.controller.js
+++ Backend/controllers/works.controller.js
@@ -1,29 +1,28 @@
 const db = require('./../models');
 const Works = db.works
 
-exports.findAll = async (req, res) =>  {
-	const works = await Works.findAll({include: 'category'});
-	return res.status(200).json(works);
-}
-
-exports.create = async (req, res) => {
-	const host = req.get('host');
-	const title = req.body.title;
-	const categoryId = req.body.category;
-	const userId = req.auth.userId;
-	const imageUrl = `${req.protocol}://${host}/images/${req.file.filename}`;
-	try{
-		const work = await Works.create({
-			title,
-			imageUrl,
-			categoryId,
-			userId
-		})
-		return res.status(201).json(work)
-	}catch (err) {
-		return res.status(500).json({ error: new Error('Something went wrong') })
+exports.findAll = async (req, res) => {
+	try {
+	  const works = await Works.findAll({ include: 'category' });
+  
+	  const host = req.get('host');
+	  const protocol = req.protocol;
+  
+	  const updatedWorks = works.map(work => {
+		const workJson = work.toJSON(); 
+		if (workJson.imageUrl && workJson.imageUrl.includes('localhost')) {
+		  workJson.imageUrl = `${protocol}://${host}/images/${workJson.imageUrl.split('/').pop()}`;
+		}
+		return workJson;
+	  });
+	  
+  
+	  return res.status(200).json(updatedWorks);
+	} catch (err) {
+	  return res.status(500).json({ error: 'Something went wrong' });
 	}
-}
+  };
+  
 
 exports.delete = async (req, res) => {
 	try{
