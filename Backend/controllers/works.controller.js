0c966edba51965d2a0be6f33754d3d78bf030c80
JonathanBret <bret.jonathan@outlook.fr>
2025-04-12 01:01:34 +0200

    fix url image for render

--- Backend/controllers/works.controller.js
+++ Backend/controllers/works.controller.js
@@ -1,10 +1,21 @@
 const db = require('./../models');
-const Works = db.works
+const Works = db.works;
 
 exports.findAll = async (req, res) => {
   try {
     const works = await Works.findAll({ include: 'category' });
-    return res.status(200).json(works);
+
+    // Modification de l'URL de l'image si elle contient 'localhost'
+    const updatedWorks = works.map(work => {
+      const workJson = work.toJSON();
+      if (workJson.imageUrl && workJson.imageUrl.includes('localhost')) {
+        // Remplacer localhost par le domaine de production
+        workJson.imageUrl = `https://${process.env.HOST}/images/${workJson.imageUrl.split('/').pop()}`;
+      }
+      return workJson;
+    });
+
+    return res.status(200).json(updatedWorks);  // Retourner la liste mise à jour
   } catch (err) {
     return res.status(500).json({ error: 'Something went wrong' });
   }
@@ -15,7 +26,7 @@ exports.create = async (req, res) => {
   const title = req.body.title;
   const categoryId = req.body.category;
   const userId = req.auth.userId;
-  const imageUrl = `${req.protocol}://${host}/images/${req.file.filename}`;
+  const imageUrl = `https://${process.env.HOST}/images/${req.file.filename}`;
 
   try {
     const work = await Works.create({
