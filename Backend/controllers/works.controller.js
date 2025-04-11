c34c0a599faac33086dabc8b5e3b68d6688182bc
JonathanBret <bret.jonathan@outlook.fr>
2025-04-12 00:43:39 +0200

    fix: url image

--- Backend/controllers/works.controller.js
+++ Backend/controllers/works.controller.js
@@ -1,27 +1,26 @@
 const db = require('./../models');
 const Works = db.works
 
-exports.findAll = async (req, res) => {
+exports.create = async (req, res) => {
+	const host = req.get('host');
+	const title = req.body.title;
+	const categoryId = req.body.category;
+	const userId = req.auth.userId;
+	const imageUrl = `${req.protocol}://${host}/images/${req.file.filename}`;
+  
 	try {
-	  const works = await Works.findAll({ include: 'category' });
-  
-	  const host = req.get('host');
-	  const protocol = req.protocol;
-  
-	  const updatedWorks = works.map(work => {
-		const workJson = work.toJSON(); 
-		if (workJson.imageUrl && workJson.imageUrl.includes('localhost')) {
-		  workJson.imageUrl = `${protocol}://${host}/images/${workJson.imageUrl.split('/').pop()}`;
-		}
-		return workJson;
+	  const work = await Works.create({
+		title,
+		imageUrl,
+		categoryId,
+		userId
 	  });
-	  
-  
-	  return res.status(200).json(updatedWorks);
+	  return res.status(201).json(work);
 	} catch (err) {
 	  return res.status(500).json({ error: 'Something went wrong' });
 	}
   };
+  
   
 
 exports.delete = async (req, res) => {
